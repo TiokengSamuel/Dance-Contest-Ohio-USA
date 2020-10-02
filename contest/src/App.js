dbf44edf18f72602eef0602584200e7ad2a125d0
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-02 16:46:56 +0100

    a

--- contest/src/App.js
+++ contest/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Nav from './Nav'
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="app">
       <Header/>
       <Nav/>
+      <Banner/>
     </div>
   );
 }
