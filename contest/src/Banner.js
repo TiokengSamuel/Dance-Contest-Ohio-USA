2f6100cdfca580f110db5fb252a8ffa5b7880b6a
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-02 18:04:37 +0100

    b

--- contest/src/Banner.js
+++ contest/src/Banner.js
@@ -4,7 +4,30 @@ import Banner from './Banner'
 function Banner() {
     return (
         <div className="banner">
-            
+            <section className="slider text-center" id="slider">
+                <div className="slider-overlay">
+                    <div className="slider-content">
+                        
+                        <br>
+                        <div className="text"></div>
+                        <br><br>
+                        <div className="cta-div">
+                            <a href="" class="btn1"><span class="con-text">Contact Us</span></a>
+                        </div>
+                        <br>
+                        <br>
+                        <div className="social-network">
+                            <a href="" class="fa fa-facebook"></a>
+                            <a href="" class="fa fa-instagram"></a>
+                            <a href="" class="fa fa-whatsapp"></a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+        </section>
         </div>
     )
 }
