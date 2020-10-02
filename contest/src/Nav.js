f9e1dbc9cb7496dd783f26bec02112e3f32affcb
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-02 12:56:08 +0100

    n

--- contest/src/Nav.js
+++ contest/src/Nav.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import './Nav.css'
 import HomeIcon from '@material-ui/icons/Home'
+import ThumbUpSharp from '@material-ui/icons/ThumbUpSharp'
 
 function Nav({setSelectedOption}) {
     return (
     <center>
         <div className="navi">
-        <div className="header__icons">
-                <div className="header__icon header__icon--active">
-                    <HomeIcon/>
-                    <p>Home</p>
-                </div>
-        </div>
+            <div className="header__icons">
+                    <div className="header__icon header__icon--active">
+                        <HomeIcon/>
+                        <p>Home</p>
+                    </div>
+            </div>
             <h2>Contest</h2>
-            <<div className="header__icons">
+            <div className="header__icons">
                 <div className="header__icon header__icon--active">
-                    <HomeIcon/>
+                    <ThumbsUpIcon/>
                     <p>Home</p>
                 </div>
-        </div>
+            </div>
             <h2>Products</h2>
             <h2>Support</h2>
         </div>
