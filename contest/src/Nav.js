77d5a7d9de4696c8ef2250243a7d2068e98958a6
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-02 09:12:55 +0100

    nav

--- contest/src/Nav.js
+++ contest/src/Nav.js
@@ -5,7 +5,11 @@ function Nav({setSelectedOption}) {
     return (
     <center>
         <div className="navi">
-            <h2>Home</h2>
+        <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <HomeIcon/>
+                    <p>Home</p>
+                </div>
             <h2>Contest</h2>
             <h2>Vote</h2>
             <h2>Products</h2>
